d0a3a1a9afec8d6a44217d5b850bb5c8ed3b39d8
unknown <Kelly Owoju>
2023-01-07 19:33:49 +0100

    initialized the project, set up the global user interface variables, created a decalaration file for ts types and finally implemented the login desktop design

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface SidebarProps {
+
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({}) => {
+    return (
+        <div>
+            Hello there
+        </div>
+    );
+}
---
+++ src/sections/LoginSection.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "../Styles/Login_styles/login.css";
+import Logo from "../assets/images/lendsqr_logo.png";
+import Illustration from "../assets/svgs/login_illustration.svg";
+
+interface LoginSectionProps {}
+
+export const LoginSection: React.FC<LoginSectionProps> = ({}) => {
+  const [show, setShow] = useState(false);
+
+  const togglePassword = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div className="Login">
+      <div className="inner_flex">
+        <div className="left">
+          <div>
+            <img src={Logo} alt="lendsqr_logo" className="logo" />
+            <div className="illustration_container">
+              <img src={Illustration} alt="" className="illustration" />
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div>
+            <p className="title">Welcome!</p>
+            <p className="subtitle">Enter details to login</p>
+            <div className="input_container">
+              <div className="custom_input">
+                <input type="email" className="input_box" placeholder="Email" />
+              </div>
+              <div className="custom_input">
+                <input
+                  type={show ? "text" : "password"}
+                  className="input_box"
+                  placeholder="Password"
+                />
+                <p className="password_toggle" onClick={() => togglePassword()}>
+                  {show ? "Hide" : "Show"}
+                </p>
+              </div>
+            </div>
+            <p className="forgot_password" >Forgot Password</p>
+            <button className="login_btn" >Log In</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
