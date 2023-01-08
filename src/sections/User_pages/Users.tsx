0192d1fefa949d0e09ba2f8128fe9203a5cbe6c0
unknown <Kelly Owoju>
2023-01-08 19:25:44 +0100

    completed the user section cards design

--- src/sections/User_pages/Users.tsx
+++ src/sections/User_pages/Users.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Sidebar } from "../../components/Sidebar";
+import "../../Styles/User_styles/Users/users.css";
+import { cards } from "../../utils/card_data";
 
 interface UserProps {}
 
 export const User: React.FC<UserProps> = ({}) => {
   return (
     <Sidebar>
-      <div>
-        <div>Welcome to Adedeji's page</div>
+      <div className="Users">
+        <div>
+          <p className="main_title">Users</p>
+          <div className="cards_container">
+            {cards.map((card, idx) => (
+              <div className="card" key={idx}>
+                <div>
+                  <img src={card.icon} alt="icon" />
+                  <p className="card_title" >{card.title}</p>
+                  <p className="card_number" >{card.number}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </Sidebar>
   );
---
+++ src/utils/card_data.tsx
@@ -0,0 +1,27 @@
+import Users_Icon from "../assets/Content_Icons/users_icon.png";
+import Active_Users_Icon from "../assets/Content_Icons/active_users_icon.png";
+import Users_With_Loan_Icon from "../assets/Content_Icons/users_with_loan.png";
+import Users_With_Savings_Icon from "../assets/Content_Icons/users_with_savings.png";
+
+export const cards = [
+  {
+    title: "USERS",
+    icon: Users_Icon,
+    number: "2,453",
+  },
+  {
+    title: "ACTIVE USERS",
+    icon: Active_Users_Icon,
+    number: "2,453",
+  },
+  {
+    title: "USERS WITH LOAN",
+    icon: Users_With_Loan_Icon,
+    number: "12,453",
+  },
+  {
+    title: "USERS WITH SAVINGS",
+    icon: Users_With_Savings_Icon,
+    number: "102,453",
+  },
+];
